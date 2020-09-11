8b4b76790850141d123946a1fb32c082eb5bfbb2
rohits777 <rohitashinde95@gmail.com>
2020-09-11 16:25:20 +0530

    Added:game continue till difference of minimum 2 points acheived

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -2,9 +2,9 @@ echo "Welcome to Flip Coin Simulation Program"
 #!/bin/bash
 head=0
 tail=0
-for (( i=1; i<=50; i++ ))
+for (( i=1; i<=42; i++ ))
 do
-	a=`echo $(($RANDOM%2))`
+	x=`echo $(($RANDOM%2))`
 	if [ $x -eq 0 ]
 	then
 		head=$(( $head + 1 ))
@@ -19,7 +19,7 @@ do
 		then
 			break
 		else
-			x=`echo $(( $RANDOM%2 ))`
+			x=`echo $(($RANDOM%2))`
 			if [[ $x -eq 0 ]]
 		        then
                 		head=$(( $head + 1 ))
@@ -28,8 +28,27 @@ do
         		fi
 			if [[ $head -eq $tail ]]
 			then
+				tie=0
 				echo -e "Tie\nHead=$head\nTail=$tail"
-				exit
+				while [ $tie -eq 0 ]
+				do
+					x=`echo $(( $RANDOM%2 ))`
+				        if [ $x -eq 0 ]
+        				then
+                				head=$(( $head + 1 ))
+        				else
+                				tail=$(( $tail + 1 ))
+        				fi
+					y=$(( $head - $tail ))
+					z=$(( $tail - $head ))
+					if [[ $y -ge 2 || $z -ge 2 ]]
+					then
+						tie=1
+					else
+						tie=0
+					fi
+				done
+				break
 			elif [[ $head > $tail ]]
 			then
 				head=$(( $head - 1 ))
